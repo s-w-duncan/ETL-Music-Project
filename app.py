e01b4bf15bc81570773a1c41c0dc9a4e54dd7768
Scott Duncan <scott.weston.duncan@gmail.com>
2020-05-13 13:15:52 -0700

    File Update

--- app.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, request
-from flask_pymongo import PyMongo
-import scrape_etl
--- scrap_etl.py
+++
@@ -1,7 +0,0 @@
-# Dependencies
-from bs4 import BeautifulSoup as bs
-from splinter import Browser
-import pandas as pd
-import os
-import time
-import requests
